feded5db703ad9b217dbf12b0b26a15d11d93245
Amaya <amaya.harris@tcu.edu>
2025-03-01 18:56:15 -0600

    Pass to child components with props.

---
+++ src/BlogPost.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="blog-post">
+    <p>
+        {{ message }}
+    </p>
+    <!--Display the id, title, and content of a blog post-->
+    <h2>{{ id }} - {{ blogPostTitle }}</h2>
+    <h4>{{ blogPostContent }}</h4>
+    <button @click="changeBlogPostTitle"> Change blog post title</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let message = ref('This is the BlogPost component.')
+let props = defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+function changeBlogPostTitle() {
+    props.blogPostTitle = 'A different title'
+}
+</script>
+
+<style scoped>
+.blog-post{
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
