bdcb9f22fd75363ff6a2356ba6e27eca964e12e3
Amaya <amaya.harris@tcu.edu>
2025-03-03 13:30:45 -0600

    Introduction to Vue slots.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div> Hello, Vue!</div>
+  <AlertBox>Invaild username or password. Please try again.</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+   <h1>
+      The request timed out. Please refresh the page and try again.
+   </h1>
+  </AlertBox>
+  
 </template>
 
 <script setup>
-
+import AlertBox from './AlertBox.vue';
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped> </style>
