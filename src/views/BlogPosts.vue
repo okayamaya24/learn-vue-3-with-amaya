c67213bfadeeeeb9d708b39af829307b57e19b3b
Amaya <amaya.harris@tcu.edu>
2025-04-15 10:23:53 -0500

    Named views

--- src/views/BlogPosts.vue
+++ src/views/BlogPosts.vue
@@ -3,8 +3,9 @@
         <aside class="blog-post-list">
             <BlogPostList></BlogPostList>
         </aside>
-        <main class="blog-post-content">
-            <router-view></router-view>
+        <main class="blog-post-content-with-sidebar">
+            <router-view class="blog-post-content"></router-view>
+            <router-view name="sidebar"></router-view>
         </main>
     </div>
 </template>
@@ -16,14 +17,20 @@ import BlogPostList from '@/Components/BlogPostList.vue'
 <style lang="scss" scoped>
 .blog-posts-layout {
     display: flex;
+    width: 100%;
     gap: 1rem;
 
     .blog-post-list {
         flex: 0 0 25%
     }
 
-    .blog-post-content {
-        flex: 0 0 75%;
+    .blog-post-content-with-sidebar {
+       display: flex; 
+       flex: 0 0 75%;
+
+        .blog-post-content {
+        flex: 1;
+        }
     }
 }
 </style>
