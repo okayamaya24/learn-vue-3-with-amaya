b630fb5a4f89f23a38bd7a27ac42ede100e5717f
Amaya <amaya.harris@tcu.edu>
2025-04-15 08:54:43 -0500

    Basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div> Hello, Vue!</div>
+  <div class="app">
+      <Navigation></Navigation>
+      <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
+import Navigation from '@/Components/Navigation.vue';
 
 </script>
 
 <style lang="scss" scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 
+}
 </style>
