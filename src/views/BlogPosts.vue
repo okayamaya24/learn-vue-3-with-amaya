0f5d69dbe057e6e28bce89ba4d9d18186362d9f2
Amaya <amaya.harris@tcu.edu>
2025-04-17 10:34:42 -0500

    Updated Components

--- src/views/BlogPosts.vue
+++ src/views/BlogPosts.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script setup>
-import BlogPostList from '@/Components/BlogPostList.vue'
+import BlogPostList from '@/components/BlogPostList.vue'
 </script>
 
 <style lang="scss" scoped>
